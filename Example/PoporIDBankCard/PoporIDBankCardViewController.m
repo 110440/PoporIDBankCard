a21a533c3cb46b0fc3b702c1166474f15cc820e0
wangkq <908891024@qq.com>
2018-08-21 15:53:08 +0800

    开始引入,有错误

--- Example/PoporIDBankCard/PoporIDBankCardViewController.m
+++ Example/PoporIDBankCard/PoporIDBankCardViewController.m
@@ -8,22 +8,107 @@
 
 #import "PoporIDBankCardViewController.h"
 
-@interface PoporIDBankCardViewController ()
+#import <Masonry/Masonry.h>
+#import "XLBankScanViewController.h"
+#import "XLIDScanViewController.h"
+
+@interface PoporIDBankCardViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, strong) UITableView * infoTV;
+@property (nonatomic, strong) NSArray * infoArray;
 
 @end
 
 @implementation PoporIDBankCardViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"OCR";
+    self.infoArray = @[@"银行卡", @"身份证"];
+    [self addTVs];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UITableView
+- (UITableView *)addTVs {
+    UITableView * oneTV = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    
+    oneTV.delegate   = self;
+    oneTV.dataSource = self;
+    
+    oneTV.allowsMultipleSelectionDuringEditing = YES;
+    oneTV.directionalLockEnabled = YES;
+    
+    oneTV.estimatedRowHeight           = 0;
+    oneTV.estimatedSectionHeaderHeight = 0;
+    oneTV.estimatedSectionFooterHeight = 0;
+    
+    [self.view addSubview:oneTV];
+    
+    [oneTV mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.mas_equalTo(UIEdgeInsetsZero);
+    }];
+    
+    return oneTV;
 }
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.infoArray.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 44;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString * CellID = @"CellID";
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:CellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellID];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    }
+    
+    cell.textLabel.text = self.infoArray[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    switch (indexPath.row) {
+        case 0:
+            [self pushBankCardOCR];
+            break;
+        case 1:
+            [self pushIDCardOCR];
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)pushBankCardOCR {
+    [self.navigationController pushViewController:[XLBankScanViewController new] animated:YES];
+}
+
+- (void)pushIDCardOCR {
+    [self.navigationController pushViewController:[XLIDScanViewController new] animated:YES];
+}
+
 
 @end
